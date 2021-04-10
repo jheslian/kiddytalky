a875afb0832f256674824ea081342188189899ad
jesusa <justinjhes@gmail.com>
2021-04-10 14:27:54 +0200

    child modification mais parent primary key est en dure

--- mykids/urls.py
+++ mykids/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, re_path
-from .views import mykid_view,  ChildRegisterView,MyChildView #UpdateChildView, DeleteChildView
+from .views import ChildRegisterView, ChildListView, MyChildView, UpdateChildView, DeleteChildView
 
 app_name = 'mykids'
 
 urlpatterns = [
-    path('', mykid_view, name='kids'),
+    path('', ChildListView.as_view(), name='kids'),
+    path('<int:id>', MyChildView.as_view(), name='child-view'),
     path('register/', ChildRegisterView.as_view(), name='childregister'),
-    # path('', mykid_view, name='kids'),
-    # path('<int:id>/edit', UpdateChildView.as_view(), name='edit-parent'),
-    # path('<int:id>/delete', DeleteChildView.as_view(), name='delete-parent'),
+    path('<int:id>/edit', UpdateChildView.as_view(), name='edit-child'),
+    path('<int:id>/delete', DeleteChildView.as_view(), name='delete-child'),
 
 ]
