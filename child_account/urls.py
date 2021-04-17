196408954ab8d3b3c5d7d1230a2e77b76d16310d
jesusa <justinjhes@gmail.com>
2021-04-17 10:51:53 +0200

    creation child app

---
+++ child_account/urls.py
@@ -0,0 +1,18 @@
+"""
+Module (name)
+
+description
+"""
+
+def main():
+    """
+    entry point of the script
+    """
+    pass
+    
+
+if __name__ == '__main__':
+    #
+    # calls the main function
+    #
+    main()    
